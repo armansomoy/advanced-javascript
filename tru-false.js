8f524630201fed1b8b8f2c8fd55db446fc88d8ab
armansomoy <armansamoy42@gmail.com>
2020-08-18 11:48:01 +0600

    second time

---
+++ tru-false.js
@@ -0,0 +1,22 @@
+const number = null;
+if( number ){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is false");
+} 
+
+// false
+// 0 
+// ""
+// undefined
+// Nan 
+// null 
+// false
+
+
+// true
+// "name"
+// " "
+// "0"
+// "null
